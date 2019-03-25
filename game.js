c93341bd8c855eb09f631e7bd749abe67f8f0d90
mazenswar <mazenswar@protonmail.com>
2019-03-25 15:53:57 -0400

    base working

--- game.js
+++ game.js
@@ -4,9 +4,9 @@ var config = {
   height: 700,
   physics: {
       default: 'arcade',
-      arcade: {
-          gravity: { y: 200 }
-      }
+      // arcade: {
+      //      gravity: { y: 200 }
+      // }
   },
   scene: {
       preload: preload,
@@ -22,6 +22,7 @@ var config = {
 // });
 var game = new Phaser.Game(config)
 let player;
+var platforms;
 
 function preload() {
   // PARAMETERS: (key, filePath, OPTIONAL -> configObject)
@@ -34,16 +35,39 @@ function preload() {
 
 function create() {
 
-  const platforms = this.physics.add.staticGroup();
 
   /* Creates a platform at x & y position
   PARAMETERS: (x-position, y-position, key)
   **  x-position:     the lower, the more to the left; higher is more to the right
   **  y-position:     the lower, the more up it goes; higher moves it down
   */
-  platforms.create(100, 715, 'platform')
-  platforms.create(356, 715, 'platform-end')
-  platforms.create(900, 715, 'platform')
+
+  platforms = this.physics.add.group({
+          key: 'platform',
+          frameQuantity: 100,
+          setXY: { x: 350, y: 500, stepX: 100},
+          velocityX: -60,
+          immovable: true,
+
+      });
+;
+
+  // platforms.getChildren()[0].setFrictionX(1);
+  // platforms.getChildren()[1].setFrictionX(0.5);
+  // platforms.getChildren()[2].setFrictionX(0);
+
+
+
+
+
+
+  // platforms = this.physics.add.group();
+  // platforms.create(100, 715, 'platform')
+  // platforms.create(356, 715, 'platform-end')
+  // // platforms.create(900, 715, 'platform')
+  //
+  // platforms.children.entries.forEach(platform => platform.setCollideWorldBounds(true) )
+  // // platforms.setCollideWorldBounds(true)
 
 
   /* Creates player and enables physics so player will fall
@@ -54,17 +78,22 @@ function create() {
   **   frame:         the beginning of the sprite to display
   */
   // const player = this.physics.add.sprite(100, 350, 'tom', 0) ||OLD CODE||
-  player = this.physics.add.sprite(100, 350, 'tom', 'run001.png')
+  player = this.physics.add.sprite(350, 250, 'tom', 'run001.png')
   // When player falls and lands on screen end, adds a bounce
-  player.setBounce(0.2);
+  // player.setBounce(0.2);
   // Prevents the player from falling through the screen
-  player.setCollideWorldBounds(true);
+
+  // player.set(200,200)
 
   // Adds the heaviness to the player, the higher the amount, the more it weighs and falls quicker
   // When physics sprite is created, it is given a body property to set gravity on
   player.body.setGravityY(200);
   // Prevents player from passing through a platform
   this.physics.add.collider(player, platforms);
+
+  // let arr = platforms.children.entries;
+  // arr.forEach(platform => platform.x += 300)
+
 
   /* Creates animation passing in an Object as an argument
   **   key:           a Key Name that describes the animation
@@ -73,6 +102,27 @@ function create() {
                       along with an Object indicating the start and end frames
   **   frameRate:     specifies the speed that the frames should run
   */
+//   this.anims.create({
+//     key: 'left',
+//     frames: this.anims.generateFrameNumbers('tom', { start: 0, end: 3 }),
+//     frameRate: 10,
+//     repeat: -1
+// });
+//
+// this.anims.create({
+//     key: 'turn',
+//     frames: [ { key: 'tom', frame: 4 } ],
+//     frameRate: 20
+// });
+//
+// this.anims.create({
+//     key: 'right',
+//     frames: this.anims.generateFrameNumbers('tom', { start: 5, end: 8 }),
+//     frameRate: 10,
+//     repeat: -1
+// });
+
+
   this.anims.create({
     key: 'run',
     repeat: -1,
@@ -99,11 +149,33 @@ function create() {
       end: 2,
       zeroPad: 3
     }),
-    frameRate: 10
+    frameRate: 2
   })
 
   // Call play() passing in the animation key previously created to play the animation
   // player.play('run'); ||OLD CODE||
+
+  // else if (cursors.right.isDown)
+  // {
+  //     player.setVelocityX(160);
+  //
+  //     player.anims.play('right', true);
+  // }
+  // else
+  // {
+  //     player.setVelocityX(0);
+  //
+  //     player.anims.play('turn');
+  // }
+  //
+  // if (cursors.up.isDown && player.body.touching.down)
+  // {
+  //     player.setVelocityY(-330);
+  // }
+
+
+
+
 
   cursors = this.input.keyboard.createCursorKeys();
 }
@@ -111,8 +183,9 @@ function create() {
 function update() {
   if (cursors.up.isDown && player.body.touching.down) {
     player.play('jump')
-    player.setVelocityY(-330);
-  } else {
-    player.play('run', true)
+    player.setVelocityY(-200);
+  } else if (player.body.touching.down) {
+    player.play('run', true);
+    player.setVelocityX(30);
   }
 }
